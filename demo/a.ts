f8929947994841e56c464e6b430d1e88add658cf
ShuangYa <i@sylingd.com>
2020-02-10 21:51:09 +0800

    demo: update demo

--- demo/a.ts
+++ demo/a.ts
@@ -2,9 +2,9 @@ import { Team, Heros } from '../src/owcode/type';
 // import '../src/owcode/type/constants';
 // import '../src/owcode/type/functions';
 
-// const a = 1;
-// let b = 2;
-// let c = 2.11111;
+const a = 1;
+let b = 2;
+let c = 2.11111;
 const d = () => {
   wait(1);
 }
@@ -13,9 +13,9 @@ export class myClass {
   @runAt(Events.GLOBAL_ONGOING)
   @condition(Game.isWaitingPlayers)
   test() {
-    // wait(a);
+    wait(a);
     d();
-    // b = b + 1;
-    // wait(b);
+    b = b + 1;
+    wait(b);
   }
 }
